ce5ae57c5a4e056594288bf14d05a04a274bbfda
Avivit Weissman <wavivit@gmail.com>
2021-05-05 05:30:49 +0000

    added stdin

--- survey.js
+++ survey.js
@@ -1,4 +1,8 @@
 const readline = require('readline');
+const stdin = process.stdin;
+
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -7,33 +11,43 @@ const rl = readline.createInterface({
 
 let profile = '';
 
-const questions = [`What's your name? Nicknames are also acceptable `,
+const allquestions = [`What's your name? Nicknames are also acceptable :) `,
 `What's an activity you like doing? `,
 `What do you listen to while doing that? `,
 `Which meal is your favourite (eg: dinner, brunch, etc.) `,
 `What\'s your favourite thing to eat for that meal? `,
-`Which sport is your absolute favourite? `,
+`Which sport is your absolute favourite?` ,
 `What is your superpower? In a few words, tell us what you are amazing at! `
 ];
 
 const arrAnswres = [];
-const i = 0;
+let i = 0;
 
-const processQuestions = function(questions){
-  rl.question(questions[i], (answer) => {
+const processQuestions = function(question){
+  rl.question(question, (answer) => {
     console.log(`Thank you for your valuable feedback: ${answer}`);
     arrAnswres.push(answer);
+    i += 1;
+
+    if (i < question.length) {
+    processQuestions(question);
+    } else {
+      //print the paragraph
+    }
 
   rl.close();
   });
-
-    if (i < questions.length) {
-    processQuestions(questions[i]);
-    i +=1;
-    }
 }
 
-processQuestions(questions);
+processQuestions(allquestions);
 
 //Devani loves listening to Ludovico Einaudi while coding, devouring Yak Momos for brunch,
 //prefers Tennis over any other sport, and is amazing at dropping mad puns at inopportune times
+
+// stdin.on('data', (key) => {
+//   process.stdout.write('.');
+//   // \u0003 maps to ctrl+c input
+// if (key === '\u0003') {
+//   process.exit();
+// }
+// });
